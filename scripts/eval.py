109b1e8ab65f1ca150188e49dcf12eb2408b405f
Dominic Jack <thedomjack@gmail.com>
2018-05-24 16:38:33 +1000

    added eval script

---
+++ scripts/eval.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+
+def eval_model(model_id):
+    import tensorflow as tf
+    from template_ffd.model import get_builder
+    tf.logging.set_verbosity(tf.logging.INFO)
+    builder = get_builder(model_id)
+    builder.initialize_variables()
+    print(builder.eval())
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'model_id', help='id of model defined in params')
+    args = parser.parse_args()
+    eval_model(args.model_id, max_steps=args.max_steps)
